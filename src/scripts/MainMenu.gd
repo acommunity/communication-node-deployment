d101c0a8dae4440dd4352a8a8e8b60bca8a35ee3
Ivan Udovin <wilcot@ya.ru>
2018-04-07 14:12:42 +0300

    Add about menu

--- src/scripts/MainMenu.gd
+++ src/scripts/MainMenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _process(delta):
-	var player = get_node("VideoPlayer")
+	var player = get_node("Video")
 
 	if !player.is_playing():
 		player.play()
@@ -12,13 +12,13 @@ func _on_Start_pressed():
 	get_tree().change_scene("res://scenes/Game.tscn")
 
 
-func _on_Exit_pressed():
-	get_tree().quit()
-
-
 func _on_Settings_pressed():
 	get_tree().change_scene("res://scenes/Settings.tscn")
 
 
 func _on_About_pressed():
 	get_tree().change_scene("res://scenes/About.tscn")
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
