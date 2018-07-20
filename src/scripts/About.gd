4dd7000fa6a9c72f2aaf1e04e9fc4a71ea181912
Ivan Udovin <wilcot@ya.ru>
2018-07-20 11:04:17 +0300

    Move settings and about to main menu

--- src/scripts/About.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-
-func _on_MainMenu_pressed():
-	get_tree().change_scene("res://scenes/menu/Main.tscn")
--- src/scripts/Main.gd
+++ src/scripts/Main.gd
@@ -13,12 +13,33 @@ func _on_Start_pressed():
 
 
 func _on_Settings_pressed():
-	get_tree().change_scene("res://scenes/menu/Settings.tscn")
+	_set_menu_scene("res://scenes/menu/Settings.tscn")
 
 
 func _on_About_pressed():
-	get_tree().change_scene("res://scenes/menu/About.tscn")
+	_set_menu_scene("res://scenes/menu/About.tscn")
 
 
 func _on_Exit_pressed():
 	get_tree().quit()
+
+
+func _set_menu_scene(resource):
+	var scene_resource = ResourceLoader.load(resource)
+
+	var scene = scene_resource.instance()
+
+	for child in get_node("MenuRect").get_children():
+		child.queue_free()
+
+	scene.get_node("Close").connect("pressed", self, "_on_Close_pressed")
+
+	get_node("MenuRect").add_child(scene)
+	get_node("MenuRect").set_visible(true)
+
+
+func _on_Close_pressed():
+	for child in get_node("MenuRect").get_children():
+		child.queue_free()
+
+	get_node("MenuRect").set_visible(false)
